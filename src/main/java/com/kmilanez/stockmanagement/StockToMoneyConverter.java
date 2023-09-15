670459f7bea47ab63c567790a8780d3f39ebc7a9
Karlos Milanez <karlos@kmilanez.com>
2023-09-15 16:52:20 +0100

    fix main class to reference right class

---
+++ src/main/java/com/kmilanez/stockmanagement/StockToMoneyConverter.java
@@ -0,0 +1,24 @@
+package com.kmilanez.stockmanagement;
+
+import java.util.List;
+
+public class StockToMoneyConverter {
+    
+    public void convert(List<String> stocks, Wallet wallet) {
+        for (String stock : stocks) {
+            if (stock == StockNames.STOCK_A) {
+                Double stockValue = StockValues.findStockValueByName(stock);
+                wallet.addMoney(stockValue.intValue());
+            } else if (stock == StockNames.STOCK_B) {
+                Double stockValue = StockValues.findStockValueByName(stock);
+                wallet.addMoney(stockValue.intValue());
+            } else if (stock == StockNames.STOCK_C) {
+                Double stockValue = StockValues.findStockValueByName(stock);
+                wallet.addMoney(stockValue.intValue());
+            } else {
+                wallet.addMoney(3);
+            }
+        }
+    }
+
+}
