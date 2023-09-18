5da0dcd8e28403e0538615449001af290e027c3f
Karlos Milanez <karlos@kmilanez.com>
2023-09-18 09:26:17 +0100

    cleaning code a bit

--- src/main/java/com/kmilanez/stockmanagement/StockManagement.java
+++ src/main/java/com/kmilanez/stockmanagement/StockManagement.java
@@ -2,10 +2,8 @@ package com.kmilanez.stockmanagement;
 
 import java.util.List;
 
-public class StockManagement 
-{
-    public static void main( String[] args )
-    {
+public class StockManagement {
+    public static void main( String[] args ) {
         Wallet wallet = new Wallet(5);
 
         List<String> stocks = List.of(
@@ -17,8 +15,5 @@ public class StockManagement
 
         StockToMoneyConverter stockToCashConverter = new StockToMoneyConverter();
         stockToCashConverter.convert(stocks, wallet);
-
-        System.out.println(wallet.getBalance());
-
     }
 }
